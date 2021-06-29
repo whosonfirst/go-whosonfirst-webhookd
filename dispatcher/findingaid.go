5cde83d890ec51cefdf914d08d24f35845369dd1
thisisaaronland <aaron@localhost>
2021-06-29 10:19:50 -0700

    improve dispatcher/findingaid.go constructor; still untested

--- dispatcher/findingaid.go
+++ dispatcher/findingaid.go
@@ -17,19 +17,22 @@ import (
 func init() {
 
 	ctx := context.Background()
-	err := webhookd_dispatcher.RegisterDispatcher(ctx, "findingaid", NewFindingAidDispatcher)
+	err := webhookd_dispatcher.RegisterDispatcher(ctx, "findingaid-repo", NewFindingAidRepoDispatcher)
 
 	if err != nil {
 		panic(err)
 	}
 }
 
-type FindingAidDispatcher struct {
+// FindingAidRepoDispatcher implements the webhookd.WebHookDispatcher and takes as input a []byte containing
+// CSV-encoded rows produced by the go-webhookd-github.GitHubCommitsTransformation package and creates a
+// corresponding go-whosonfirst-findingaid/repo.FindingAidResponse record for each row.
+type FindingAidRepoDispatcher struct {
 	webhookd.WebhookDispatcher
 	indexer findingaid.Indexer
 }
 
-func NewFindingAidDispatcher(ctx context.Context, uri string) (webhookd.WebhookDispatcher, error) {
+func NewFindingAidRepoDispatcher(ctx context.Context, uri string) (webhookd.WebhookDispatcher, error) {
 
 	u, err := url.Parse(uri)
 
@@ -39,15 +42,30 @@ func NewFindingAidDispatcher(ctx context.Context, uri string) (webhookd.WebhookD
 
 	q := u.Query()
 
-	indexer_uri := q.Get("indexer")
+	cache_uri := q.Get("cache")
 
-	indexer, err := findingaid.NewIndexer(ctx, indexer_uri)
-
-	if err != nil {
-		return nil, fmt.Errorf("Failed to create indexer for '%s', %w", indexer_uri, err)
+	if cache_uri == "" {
+		return nil, fmt.Errorf("missing ?cache= parameter")
 	}
 
-	d := FindingAidDispatcher{
+	idx_q := url.Values{}
+
+	idx_q.Set("cache", cache_uri)    // where the findingaid records will be written to
+	idx_q.Set("iterator", "null://") // no-op
+
+	idx_u := url.URL{}
+	idx_u.Scheme = "repo"
+	idx_u.RawQuery = idx_q.Encode()
+
+	idx_uri := idx_u.String()
+
+	indexer, err := findingaid.NewIndexer(ctx, idx_uri)
+
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create indexer for '%s', %w", idx_uri, err)
+	}
+
+	d := FindingAidRepoDispatcher{
 		indexer: indexer,
 	}
 
@@ -56,11 +74,13 @@ func NewFindingAidDispatcher(ctx context.Context, uri string) (webhookd.WebhookD
 
 // Dispatch takes as input a []byte containing CSV-encoded rows produced by the go-webhookd-github.GitHubCommitsTransformation
 // package and updates (or creates) a corresponding go-whosonfirst-findingaid record for each row.
-func (d *FindingAidDispatcher) Dispatch(ctx context.Context, body []byte) *webhookd.WebhookError {
+func (d *FindingAidRepoDispatcher) Dispatch(ctx context.Context, body []byte) *webhookd.WebhookError {
 
 	br := bytes.NewReader(body)
 	csv_r := csv.NewReader(br)
 
+	// TBD: Do this concurrently ?
+	
 	for {
 		row, err := csv_r.Read()
 
@@ -77,13 +97,12 @@ func (d *FindingAidDispatcher) Dispatch(ctx context.Context, body []byte) *webho
 		if err != nil {
 			return &webhookd.WebhookError{Code: 999, Message: err.Error()}
 		}
-
 	}
 
 	return nil
 }
 
-func (d *FindingAidDispatcher) dispatchRow(ctx context.Context, row []string) error {
+func (d *FindingAidRepoDispatcher) dispatchRow(ctx context.Context, row []string) error {
 
 	count_cols := len(row)
 	count_expected := 3
@@ -92,7 +111,8 @@ func (d *FindingAidDispatcher) dispatchRow(ctx context.Context, row []string) er
 		return fmt.Errorf("Invalid column count for row. Expected %d columns but row has %d", count_expected, count_cols)
 	}
 
-	// PLEASE VALIDATE THESE...
+	// TBD: More validation (go-whosonfirst-repo?) or just assume that if
+	// path is parseable, below, that everything else is probably okay?
 
 	// commit_hash := row[0]
 	repo_name := row[1]
@@ -106,7 +126,7 @@ func (d *FindingAidDispatcher) dispatchRow(ctx context.Context, row []string) er
 
 	// Basically we spoofing something that can be read by
 	// go-whosonfirst-findingaid/repo.FindingAidResponseFromReader
-
+	
 	type Feature struct {
 		Type       string                 `json:"type"`
 		Properties map[string]interface{} `json:"properties"`
@@ -129,6 +149,5 @@ func (d *FindingAidDispatcher) dispatchRow(ctx context.Context, row []string) er
 	}
 
 	fr := bytes.NewReader(enc_f)
-
 	return d.indexer.IndexReader(ctx, fr)
 }
